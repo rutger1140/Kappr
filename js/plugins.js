cc494550376326c12533b604259045746e0a6ec0
RL <lekkerduidelijk@gmail.com>
2012-02-15 23:24:20 +0100

    Importing boilerplate and first concept version of template

---
+++ js/plugins.js
@@ -0,0 +1,33 @@
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+
+// make it safe to use console.log always
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+
+
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+(function($) {
+  $.fn.extend({
+    sticky: function() {
+      var header = $(this),
+          origTop = header.offset().top,
+          content = header.add("#main");
+
+      $(window).scroll(function(e) {
+        if ($(this).scrollTop() > origTop) {
+          content.addClass("sticky");
+        } else if ($(this).scrollTop() < origTop) {
+          content.removeClass("sticky");
+        }
+      });
+    }
+  });
+
+  $(function() {
+    $('#access').sticky();
+  });
+}(jQuery));
+
