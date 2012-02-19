c0e43fe782291d4869de68545835716e426199ca
RL <lekkerduidelijk@gmail.com>
2012-02-19 17:37:37 +0100

    Template development

--- js/script.js
+++ js/script.js
@@ -7,6 +7,56 @@
 
 
 
-(function($) {
-  
+$(function() {
+
+  console.log("Loaded");
+
+
+  GmapLoad();
+
 });
+
+var map;
+
+function GmapInit() {
+  var kappr = new google.maps.LatLng(52.054153,5.079889);
+  var mapcenter = new google.maps.LatLng(52.054153,5.077989);
+  var myOptions = {
+    zoom: 16,
+    center: mapcenter,
+    disableDefaultUI: true,
+    disableDoubleClickZoom: true,
+    draggable: true,
+    scrollwheel: false,
+    overviewMapControl: false,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL,
+      position: google.maps.ControlPosition.RIGHT_BOTTOM
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+
+
+  var kapprIcon = "img/mapicon.png";
+
+  marker = new google.maps.Marker({
+    map:map,
+    draggable:true,
+    animation: google.maps.Animation.DROP,
+    position: kappr,
+    icon:kapprIcon
+  });
+  $("#locatie").addClass("hasmap");
+    //google.maps.event.addListener(marker, 'click', toggleBounce);
+
+}
+
+function GmapLoad() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=GmapInit";
+  document.body.appendChild(script);
+}
+
